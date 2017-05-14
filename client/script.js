c76bf2fd424117a70d295f7f29e5afa041f05ce2
QuanAVuong <quan.a.vuong@gmail.com>
2017-05-14 13:15:07 -0400

    search and map addresses

--- client/script.js
+++ client/script.js
@@ -7,6 +7,62 @@ $(document).ready(function(){
     zoom: 13,
   });
   
-  
+  var latlng1;
+  var latlng2;
+
+  $('#loc1').keypress(function(e){
+    if (e.which == 13) {
+      var address = $('#loc1').val();
+      GMaps.geocode({
+        address: address,
+        callback: function(results, status) {
+          if (status == 'OK') {
+            latlng1 = results[0].geometry.location;
+
+            console.log( e.target.value + " " + latlng1.lat());
+            console.log( e.target.value + " " + latlng1.lng());
+
+            mapObj.addMarker({
+              lat: latlng1.lat(),
+              lng: latlng1.lng()
+            })
+
+            mapObj.setCenter(latlng1.lat(), latlng1.lng());
+            mapObj.setZoom(13);
+          } else if (status == 'ZERO_RESULTS') {
+            alert('Sorry, no location named ' + address);
+          }
+        }
+      });
+
+    }
+  });
+
+  $('#loc2').keypress(function(e){
+    if (e.which == 13) {
+      var address = $('#loc2').val();
+      GMaps.geocode({
+        address: address,
+        callback: function(results, status) {
+          if (status == 'OK') {
+            latlng2 = results[0].geometry.location;
+
+            mapObj.addMarker({
+              lat: latlng2.lat(),
+              lng: latlng2.lng()
+            })
+
+            mapObj.setCenter(latlng2.lat(), latlng2.lng());
+            mapObj.setZoom(13);
+          } else if (status == 'ZERO_RESULTS') {
+            alert('Sorry, no location named ' + address);
+          }
+        }
+      });
+    }
+  });
+
+
+
 
 });
